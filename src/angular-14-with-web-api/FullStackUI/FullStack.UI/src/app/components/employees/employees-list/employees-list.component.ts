41707778643bd09c06c7ae006636e38d4e189b7f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-09 02:45:01 +0600

    EmployeeList added which shows static data

--- src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
 
+export class EmployeesListComponent implements OnInit {
+
+  employees: Employee[] = [
+    {
+       id: '1cedbe4f-96ae-443b-9df1-74de429cf9da',
+       name: 'John Doe',
+       email: 'john.doe@email.com',
+       phone: 998877665,
+       salary: 60000,
+       department: 'HR'
+    },
+    {
+      id: '51d1aeb2-cc58-4601-9be5-383ae888e3e2',
+      name: 'Samin Yasar',
+      email: 'samin.yasar@email.com',
+      phone: 895577665,
+      salary: 70000,
+      department: 'Marketing'
+   }
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
