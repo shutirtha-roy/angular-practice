ee9dc1bdc036f05a87b3f875780d3c2eedbc528a
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-09 21:06:46 +0600

    AddEmployeeImplemented

--- src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/app.module.ts
+++ src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesListComponent
+    EmployeesListComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
