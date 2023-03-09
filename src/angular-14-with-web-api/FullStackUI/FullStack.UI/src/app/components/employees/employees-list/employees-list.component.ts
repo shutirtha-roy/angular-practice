583ad32c8ef1340a1eb50fc13a40b31bfe7595a1
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-09 20:03:43 +0600

    GetAllEmployees Implemented

--- src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -10,29 +11,22 @@ import { Employee } from 'src/app/models/employee.model';
 export class EmployeesListComponent implements OnInit {
 
   employees: Employee[] = [
-    {
-       id: '1cedbe4f-96ae-443b-9df1-74de429cf9da',
-       name: 'John Doe',
-       email: 'john.doe@email.com',
-       phone: 998877665,
-       salary: 60000,
-       department: 'HR'
-    },
-    {
-      id: '51d1aeb2-cc58-4601-9be5-383ae888e3e2',
-      name: 'Samin Yasar',
-      email: 'samin.yasar@email.com',
-      phone: 895577665,
-      salary: 70000,
-      department: 'Marketing'
-   }
   ];
 
-  constructor() {
+  constructor(private employeesService: EmployeesService) {
 
   }
 
   ngOnInit(): void {
-      
+      this.employeesService.getAllEmployees()
+      .subscribe({
+        next: (employees) => {
+          //console.log(employees);
+          this.employees = employees;
+        },
+        error: (response) => {
+          console.log(response)
+        }
+      });
   }
 }
--- src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/services/employees.service.ts
+++ src/angular-14-with-web-api/FullStackUI/FullStack.UI/src/app/services/employees.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmployeesService {
+export class EmployeesService {  
+  private employeesUrl : string = "/api/Employees";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.employeesUrl);
+  }
 }
