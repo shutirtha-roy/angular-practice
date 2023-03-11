8dd975c5ac549adf551a3bbd76e81cf8127bf0a7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-11 19:28:59 +0600

    Login and Signup Page Template Added

--- src/angular-14-login-and-signup-page/AngularAuth/src/app/components/login/login.component.ts
+++ src/angular-14-login-and-signup-page/AngularAuth/src/app/components/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 }
