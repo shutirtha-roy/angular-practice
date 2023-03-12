da7ec2cc6ea0fc98bb6e618953a56388b42f80f0
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-12 14:09:51 +0600

    SignUp Validation Added (Reactive Form)

--- src/angular-14-login-and-signup-page/AngularAuth/src/app/components/signup/signup.component.ts
+++ src/angular-14-login-and-signup-page/AngularAuth/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,15 +11,47 @@ export class SignupComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName:  ['', Validators.required],
+      email:  ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   hideShowPass() {
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSignUp() {
+    if(this.signUpForm.valid) {
+      //Send the obj to database
+      console.log(this.signUpForm.value);
+    }
+
+    //console.log(this.loginForm.value);
+    //if not valid throw the error using toaster and required fields
+    this.validateAllFormFields(this.signUpForm);
+    alert("Your form is invalid");
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if(control instanceof FormControl) {
+        control.markAsDirty({onlySelf:true});
+      } 
+      else if(control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    })
   }
 }
