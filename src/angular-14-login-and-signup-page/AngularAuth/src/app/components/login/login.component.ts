cf2b179e19819914ab68291ee6ed51ab664e0f9c
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-12 13:33:06 +0600

    Login Validation Added (Reactive Form)

--- src/angular-14-login-and-signup-page/AngularAuth/src/app/components/login/login.component.ts
+++ src/angular-14-login-and-signup-page/AngularAuth/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,44 @@ export class LoginComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   hideShowPass() {
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      //Send the obj to database
+      console.log(this.loginForm.value);
+    }
+
+    //console.log(this.loginForm.value);
+    //if not valid throw the error using toaster and required fields
+    this.validateAllFormFields(this.loginForm);
+    alert("Your form is invalid");
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if(control instanceof FormControl) {
+        control.markAsDirty({onlySelf:true});
+      } 
+      else if(control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    })
   }
 }
