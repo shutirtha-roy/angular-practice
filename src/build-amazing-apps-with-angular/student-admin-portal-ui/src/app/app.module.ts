76971d1a8567d26dc63d1a1846a4c68d233b06cf
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-13 20:01:13 +0600

    material added

--- src/build-amazing-apps-with-angular/student-admin-portal-ui/src/app/app.module.ts
+++ src/build-amazing-apps-with-angular/student-admin-portal-ui/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -90,6 +90,9 @@ import { MatTableModule } from '@angular/material/table';
     MatPaginatorModule,
     MatSortModule,
     MatTableModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
