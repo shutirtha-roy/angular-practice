27131a9b011e30320560711dfea2efbfd59a4d6f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-15 22:20:57 +0600

    SQL Connection

--- src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
+++ src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
@@ -1,6 +1,17 @@
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+var assemblyName = Assembly.GetExecutingAssembly().FullName;
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(connectionString,
+        m => m.MigrationsAssembly(assemblyName)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
