20fd5a57065b79dd6346fb847bcc996e1d9a3438
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-16 17:57:42 +0600

    CORS Added

--- src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
+++ src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
@@ -9,12 +9,16 @@ var connectionString = builder.Configuration.GetConnectionString("DbConnection")
 builder.Services.AddDbContext<DataContext>(options =>
         options.UseSqlServer(connectionString));
 
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
