bed2f11d34e57ac84b79ddfe65f8fd8f4faee6ab
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-05-01 18:01:46 +0600

    ExceptionMiddleware Implemented

--- src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
+++ src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
@@ -1,57 +1,68 @@
 using DatingApp.API.Data;
 using DatingApp.API.Extentions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using DatingApp.API.Middleware;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DbConnection");
-
-builder.Services.AddDbContext<DataContext>(options =>
-        options.UseSqlServer(connectionString));
-
-builder.Services.AddCors();
-
-builder.Services.AddControllers();
-
-builder.Services.AddApplicationServices(builder.Configuration);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(option =>
+try
 {
-    option.AddDefaultPolicy(
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        }
-    );
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentityServices(builder.Configuration);
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DbConnection");
 
-var app = builder.Build();
+    builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(connectionString));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+    builder.Services.AddCors();
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddApplicationServices(builder.Configuration);
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddCors(option =>
+    {
+        option.AddDefaultPolicy(
+            policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            }
+        );
+    });
+
+    builder.Services.AddIdentityServices(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    app.UseMiddleware<ExceptionMiddleware>();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+
+
+    app.UseHttpsRedirection();
+
+    app.UseCors();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Console.WriteLine(ex.Message);
 }
 
-app.UseHttpsRedirection();
-
-app.UseCors();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
