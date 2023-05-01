dfa90d99ecaaef6ed2be9590ce6885ee87e37092
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-05-01 18:08:34 +0600

    Position changed

--- src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
+++ src/angular-15-dating-app/DatingApp/DatingApp.API/Program.cs
@@ -4,65 +4,55 @@ using DatingApp.API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 
-try
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+
+builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(connectionString));
+
+builder.Services.AddCors();
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(option =>
 {
-    var builder = WebApplication.CreateBuilder(args);
+    option.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+    );
+});
 
-    // Add services to the container.
-    var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddIdentityServices(builder.Configuration);
 
-    builder.Services.AddDbContext<DataContext>(options =>
-            options.UseSqlServer(connectionString));
+var app = builder.Build();
 
-    builder.Services.AddCors();
-
-    builder.Services.AddControllers();
-
-    builder.Services.AddApplicationServices(builder.Configuration);
-
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    builder.Services.AddCors(option =>
-    {
-        option.AddDefaultPolicy(
-            policy =>
-            {
-                policy.AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-            }
-        );
-    });
-
-    builder.Services.AddIdentityServices(builder.Configuration);
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
     app.UseMiddleware<ExceptionMiddleware>();
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-
-
-    app.UseHttpsRedirection();
-
-    app.UseCors();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
